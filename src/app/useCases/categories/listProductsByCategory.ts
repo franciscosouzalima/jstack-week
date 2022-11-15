ae5f7c09737dcdb0cffe8cec84f6279ec9a84fec
Francisco Lima <franciscosouzalima@gmail.com>
2022-11-15 18:16:08 -0300

    add listProductsByCategory and fix createCategory

---
+++ src/app/useCases/categories/listProductsByCategory.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+import { Product  } from '../../models/Product';
+
+export async function listProductsByCategory(req: Request, res: Response) {
+
+  try {
+    const { categoryId } = req.params;
+    const products = await Product.find({category: categoryId});
+
+
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: {
+        data: products,
+      },
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
